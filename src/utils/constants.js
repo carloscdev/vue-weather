3053e5acdcaccd5d6f0b14e7c9449ea536e5dd21
Carlos Córdova <46693089+carloscdev@users.noreply.github.com>
2023-10-20 11:54:07 -0500

    Add new countries

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,7 @@
 export const paises = [
   { codigo: 'PE', nombre: 'Perú' },
+  { codigo: 'BO', nombre: 'Bolivia' },
+  { codigo: 'CL', nombre: 'Chile' },
   { codigo: 'MX', nombre: 'México' },
   { codigo: 'US', nombre: 'Estados Unidos' },
   { codigo: 'AR', nombre: 'Argentina' },
